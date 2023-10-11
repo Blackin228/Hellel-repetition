49be1e91260120040e8f95bffd31f4bc74836346
Blackin228 <olegsorochinskiy@gmail.com>
2023-10-11 23:05:06 +0300

    Fixed dz_17.py

--- dz_17.py
+++ dz_17.py
@@ -6,7 +6,7 @@ with open('dz_16.json') as file_1:
     data = json.load(file_1)
     with open('dz_17.csv', 'w') as file_2:
         writer = csv.writer(file_2)
-        writer.writerow(['Number', 'Name', 'Age', "Tel"])
+        writer.writerow(['Number', 'Name', 'Age', 'Tel'])
         for key, item in data.items():
             row = [str(key), item[0], str(item[1]), random.randint(000000, 999999)]
             writer.writerow(row)
